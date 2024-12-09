5f1e63d01b3c4efd3384174e8ae63b3c7c447cdb
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-09 23:05:36 +0600

    add a menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
+
 const Navbar = () => {
+
+  const menu = (
+    <>
+    <li>
+      <Link href="/">Home</Link>
+    </li>
+    <li>
+      <Link href="/">Live Score</Link>
+    </li>
+    <li>
+      <Link href="/">Best Performer</Link>
+    </li>
+    </>
+  )
   return (
 <div className="navbar bg-base-100">
   <div className="navbar-start">
@@ -22,32 +38,14 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      {menu}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Goli Cricket</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {menu}
     </ul>
   </div>
   <div className="navbar-end">
