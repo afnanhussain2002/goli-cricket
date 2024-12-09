e21714eeef4105e822e236306b8afb5eb897363c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-09 23:30:36 +0600

    add a simple hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
-
 function Home() {
   return (
-    <div className="hero bg-[url('/images/hero.jpg')] min-h-screen ">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 bg-black opacity-50"></div> {/* Overlay */}
+      <div className="hero bg-[url('/images/hero.jpg')] min-h-screen relative">
+        <div className="hero-content text-center relative bg-black bg-opacity-50">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Goli Cricket</h1>
+            <p className="py-6">
+            Track Every Run, Count Every Over, Own Every Match!
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   )
 }
 
