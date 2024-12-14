a68c9ad912783d69f88775c2f72582e2928b9cd8
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-14 07:13:27 +0600

    write the schema for user

---
+++ src/model/user.model.ts
@@ -0,0 +1,22 @@
+const userSchema ={
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+}
