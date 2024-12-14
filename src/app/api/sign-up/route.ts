39c11f6f5b7c9ec1c0dbfd2eb7abe283361dd966
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-14 08:37:44 +0600

    write a condition on if username or email already exists

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -4,7 +4,16 @@ import UserModel from "@/model/user.model"
 
 export async function POST(request: Request) {
     await dbConnect()
-    const {username, email} = await request.json()
-    const user = await UserModel.create({username, email})
+   try {
+     const {username, email} = await request.json()
+
+     if (username || email) {
+        return Response.json({error: "Username or email already exists"}, {status: 400})
+     }
+     const user = await UserModel.create({username, email})
+
+   } catch (error) {
+    console.log("Error registering user", error);
+   }
     
 }
