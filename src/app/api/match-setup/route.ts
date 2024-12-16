91f8b9192caa7e56cbff71eac2c2d8bddd8de812
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-16 12:30:21 +0600

    get the all data and add a condition for mandetory data

--- src/app/api/match-setup/route.ts
+++ src/app/api/match-setup/route.ts
@@ -4,6 +4,16 @@ export async function POST(request: Request) {
 
     await dbConnect()
 
-    
+    try {
+        const {team1,team2,over,dateTime,location,ballType} = await request.json()
+
+        if(!team1 || !team2 || !over ){
+            return Response.json({error: "Missing team1, team2 or over"}, {status: 400})
+        }
+    } catch (error) {
+       console.log(error); 
+    }
+
+
 }
 
