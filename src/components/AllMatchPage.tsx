896aef9e715604c43bc80ff974ed9d09c749cfdd
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-25 11:56:24 +0600

    add three buttons on all matches

--- src/components/AllMatchPage.tsx
+++ src/components/AllMatchPage.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 function AllMatchPage() {
   return (
     <div>
-        <button>All Matches</button>
+        <div className='p-5 space-x-2'>
+        <button className='btn btn-primary'>All Matches</button>
+        <button className='btn btn-primary'>Upcoming Matches</button>
+        <button className='btn btn-primary'>Live Matches</button>
+        </div>
     </div>
   )
 }
