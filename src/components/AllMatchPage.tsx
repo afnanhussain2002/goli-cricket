91a38eee2a687a1e2edc6eafaef2f2196a3f3661
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-25 12:32:43 +0600

    show all data on frontend

--- src/components/AllMatchPage.tsx
+++ src/components/AllMatchPage.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
+"use client"
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function AllMatchPage() {
+
+    const [allMatches, setAllMatches] =useState([])
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+            try {
+                const response = await axios.get('/api/get-matches');
+                console.log("response=====", response.data);
+                setAllMatches(response.data.matches);
+            } catch (error) {
+                console.error("Error fetching matches:", error);
+            }
+        };
+    
+        fetchMatches();
+    }, []);
+
+
   return (
     <div>
         <div className='p-5 space-x-2'>
         <button className='btn btn-primary'>All Matches</button>
         <button className='btn btn-primary'>Upcoming Matches</button>
         <button className='btn btn-primary'>Live Matches</button>
+        </div>
+
+        <div>
+            {allMatches?.map((match) => (
+                <div key={match._id} className='p-5 space-x-2'>
+                    <p>{match.team1}</p>
+                    <p>{match.team2}</p>
+                    <p>{match.over}</p>
+                    <p>{match.dateTime}</p>
+                    <p>{match.location}</p>
+                    <p>{match.ballType}</p>
+                </div>
+            ))}
         </div>
     </div>
   )
